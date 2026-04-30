433e3c67513d22d2b3b13fe3431def80798f2cbb
Yury <urikbunin97@gmail.com>
2026-05-01 00:23:36 +0300

    Change progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,7 @@ def progression():
     row = [start + step * index for index in range(length)]
 
     random_index = random.randint(0, length - 1)
-    answer, row[random_index] = row[random_index], "..."
+    answer, row[random_index] = row[random_index], ".."
 
     question = ' '.join(str(x) for x in row)
     correct_answer = str(answer)
