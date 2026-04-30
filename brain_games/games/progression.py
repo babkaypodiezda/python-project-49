4d1c3a915a0fb57b9249d67ff1118e86d823f171
Yury <urikbunin97@gmail.com>
2026-05-01 00:20:59 +0300

    Change progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,20 +1,15 @@
 import random
 
-
-def row_numbers():
-   length = random.randint(5, 10)
-   start, step = random.randint(1, 30), random.randint(1, 30)
-   row = [start + step * index for index in range(length)]
-
-   random_index = random.randint(0, length - 1)
-   answer, row[random_index] = row[random_index], "..."
-   row = ' '.join(str(x) for x in row)
-   return answer, row
-
  
 def progression():
-  answer, row = row_numbers()
-  question = row
-  correct_answer = str(answer)
+    length = random.randint(5, 10)
+    start, step = random.randint(1, 30), random.randint(1, 30)
+    row = [start + step * index for index in range(length)]
 
-  return question, correct_answer
+    random_index = random.randint(0, length - 1)
+    answer, row[random_index] = row[random_index], "..."
+
+    question = ' '.join(str(x) for x in row)
+    correct_answer = str(answer)
+
+    return question, correct_answer
